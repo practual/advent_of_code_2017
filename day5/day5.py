ab517d293d805fa1da3a938068e7de817b29b8f0
Phil Richman <philip.richman@udemy.com>
2017-12-05 10:21:18 +0000

    Day 5

---
+++ day5/day5.py
@@ -0,0 +1,40 @@
+#! /usr/bin/env python
+
+# http://adventofcode.com/2017/day/4
+
+import os
+
+
+def count_steps_until_exit_part_1(programme):
+    programme = programme[:]
+    steps = 0
+    pointer = 0
+    while 0 <= pointer < len(programme):
+        steps += 1
+        offset = programme[pointer]
+        programme[pointer] += 1
+        pointer += offset
+    return steps
+
+
+def count_steps_until_exit_part_2(programme):
+    programme = programme[:]
+    steps = 0
+    pointer = 0
+    while 0 <= pointer < len(programme):
+        steps += 1
+        offset = programme[pointer]
+        programme[pointer] += -1 if offset >= 3 else 1
+        pointer += offset
+    return steps
+
+
+if __name__ == '__main__':
+    current_dir = os.path.dirname(os.path.realpath(__file__))
+    input_file_name = os.path.join(current_dir, 'input')
+
+    with open(input_file_name, 'r') as file:
+        programme = [int(instruction) for instruction in file.read().split('\n') if instruction]
+
+    print(count_steps_until_exit_part_1(programme))
+    print(count_steps_until_exit_part_2(programme))
