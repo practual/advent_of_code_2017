f028d1c2183d5f3e1d24e48d524a4c2262d87c9a
Phil Richman <philip.richman@udemy.com>
2017-12-06 14:16:30 +0000

    Simpler solution, forgot we'd already done one complete loop

--- day6/day6.py
+++ day6/day6.py
@@ -5,37 +5,22 @@
 import os
 
 
-def _redistribute_blocks(banks):
-    blocks_to_redistribute = max(banks)
-    bank_index = banks.index(blocks_to_redistribute)
-    banks[bank_index] = 0
-    while blocks_to_redistribute:
-        bank_index = (bank_index + 1) % len(banks)
-        banks[bank_index] += 1
-        blocks_to_redistribute -= 1
-
-
 def count_steps_until_repeat(banks):
     # Make a copy
     banks = banks[:]
-    seen_configs = set()
+    seen_configs = []
     steps = 0
     while tuple(banks) not in seen_configs:
-        seen_configs.add(tuple(banks))
+        seen_configs.append(tuple(banks))
         steps += 1
-        _redistribute_blocks(banks)
-    return steps, banks
-
-
-def count_steps_in_loop(banks):
-    # Make a copy
-    banks = banks[:]
-    starting_point = tuple(banks)
-    steps = 0
-    while tuple(banks) != starting_point or steps == 0:
-        steps += 1
-        _redistribute_blocks(banks)
-    return steps
+        blocks_to_redistribute = max(banks)
+        bank_index = banks.index(blocks_to_redistribute)
+        banks[bank_index] = 0
+        while blocks_to_redistribute:
+            bank_index = (bank_index + 1) % len(banks)
+            banks[bank_index] += 1
+            blocks_to_redistribute -= 1
+    return steps, steps - seen_configs.index(tuple(banks))
 
 
 if __name__ == '__main__':
@@ -45,6 +30,4 @@ if __name__ == '__main__':
     with open(input_file_name, 'r') as file:
         banks = [int(instruction) for instruction in file.read().split('\t') if instruction]
 
-    steps, loop_starting_point = count_steps_until_repeat(banks)
-    print(steps)
-    print(count_steps_in_loop(loop_starting_point))
+    print(count_steps_until_repeat(banks))
