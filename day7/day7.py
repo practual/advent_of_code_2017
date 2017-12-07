2931f8d5ef87b2c4c7ca711f97ac4340f1964cc6
Phil Richman <philip.richman@udemy.com>
2017-12-07 14:00:17 +0000

    Cleanup for day 7

--- day7/day7.py
+++ day7/day7.py
@@ -7,7 +7,7 @@ import re
 from collections import Counter, defaultdict
 
 
-def make_reverse_map(programme_map):
+def find_root(programme_map):
     # reverse map holds child -> parent maps. A blank string is used if the parent is not known.
     reverse = defaultdict(str)
     for parent, details in programme_map.items():
@@ -15,19 +15,13 @@ def make_reverse_map(programme_map):
         reverse[parent] = '' or reverse[parent]
         for child in details['children']:
             reverse[child] = parent
-    return reverse
-
-
-def find_root(child_to_parent_map):
     # Follow a child up through its parents until a child with no parent is found.
     # This is going to be faster on average than looping through all the values assuming the
     # tree is reasonably well balanced.
     # `parent` is still defined from the previous loop so we'll use that as a starting point.
-    child = list(child_to_parent_map.keys())[0]
-    parent = child_to_parent_map[child]
     while parent != '':
         child = parent
-        parent = child_to_parent_map[child]
+        parent = reverse[child]
     return child
 
 
@@ -36,23 +30,26 @@ def get_child_weight(programme_map, parent):
     for child in programme_map[parent]['children']:
         child_weights[child] = get_child_weight(programme_map, child)
 
-    if len(set(child_weights.values())) > 1:
-        # One child has a mismatched weight. Problem doesn't make sense unless we have three or
-        # more children (otherwise we couldn't say that 'only one' value needs to change - if there
-        # were only two children, we could change either one.
-        weights_to_children = {}
-        for child, weight in child_weights.items():
-            weights_to_children[weight] = child
-        weight_counter = Counter(child_weights.values())
-        problem_weight = min(weight_counter, key=weight_counter.get)
-        problem_child = weights_to_children[problem_weight]
-        target_weight = weight_counter.most_common(1)[0][0]
-        updated_weight = programme_map[problem_child]['weight'] + target_weight - problem_weight
-        raise Exception(
-            '{} is the wrong weight. Is {} but should be {}'.format(
-                problem_child, problem_weight, updated_weight)
-        )
-    return sum(child_weights.values()) + programme_map[parent]['weight']
+    if len(set(child_weights.values())) <= 1:
+        return sum(child_weights.values()) + programme_map[parent]['weight']
+
+    # One child has a mismatched weight. Problem doesn't make sense unless we have three or
+    # more children (otherwise we couldn't say that 'only one' value needs to change - if there
+    # were only two children, we could change either one.
+    weights_to_children = {}
+    for child, weight in child_weights.items():
+        # This may override an existing value, but that's fine - we'll only lookup a weight
+        # that we know has a single child for it.
+        weights_to_children[weight] = child
+    weight_counter = Counter(child_weights.values())
+    problem_weight = min(weight_counter, key=weight_counter.get)
+    problem_child = weights_to_children[problem_weight]
+    target_weight = weight_counter.most_common(1)[0][0]
+    updated_weight = programme_map[problem_child]['weight'] + target_weight - problem_weight
+    raise Exception(
+        '{} is the wrong weight. Is {} but should be {}'.format(
+            problem_child, programme_map[problem_child]['weight'], updated_weight)
+    )
 
 
 def run():
@@ -78,9 +75,7 @@ def run():
             children = []
         programme_map[parent] = {'weight': int(weight), 'children': children}
 
-    reverse_map = make_reverse_map(programme_map)
-
-    root = find_root(reverse_map)
+    root = find_root(programme_map)
     print(root)
 
     get_child_weight(programme_map, root)
